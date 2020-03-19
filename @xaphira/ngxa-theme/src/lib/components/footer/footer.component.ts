590aa4179b493157b5235fa262fa41b9dfbd7885
Ridla Fadilah <ridla.fadilah@gmail.com>
2020-03-19 16:22:39 +0700

    initialize library

--- @xaphira/ngxa-theme/src/lib/components/footer/footer.component.ts
+++ @xaphira/ngxa-theme/src/lib/components/footer/footer.component.ts
@@ -4,13 +4,7 @@ import { Component } from '@angular/core';
   selector: 'xa-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2019</span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+  <span class="created-by">© 2020 <b><a href="#" target="_blank">Civilians Emergency Report</a></b></span>
   `,
 })
 export class FooterComponent {
